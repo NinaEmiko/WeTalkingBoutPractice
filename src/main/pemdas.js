f2a2aafab289eefd86db61b20f1fff7002f82583
Nina Emiko <nina.emiko.mcnair@gmail.com>
2023-05-28 12:40:38 -0400

    Completed pemdas test

---
+++ src/main/pemdas.js
@@ -0,0 +1,9 @@
+//Given the below function, add parenthesis to the expression so that it returns 40;
+//Do not change any of the numbers
+//Do not add numbers
+
+function pemdas() {
+    return 2 + 6 * 5;
+    }
+    
+    module.exports = pemdas
---
+++ src/test/pemdas.test.js
@@ -0,0 +1,5 @@
+const pemdas = require('../main/pemdas')
+
+test('should return 40', () =>{
+    expect(pemdas()).toBe(40)
+})
