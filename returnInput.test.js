0af372475f81b5b77e88f72b6bc31534c01cb2d1
Nina Emiko <nina.emiko.mcnair@gmail.com>
2023-05-28 11:42:17 -0400

    Completed tests for returnInput

--- returnInput.test.js
+++ returnInput.test.js
@@ -1,5 +1,13 @@
 const returnInput = require('./returnInput')
 
-test('should return a value given to it', () =>{
+test('should return 17 when given 17 as a parameter', () =>{
     expect(returnInput(17)).toBe(17)
+})
+
+test('should return "Happy" when given "Happy" as a parameter', () =>{
+    expect(returnInput("Happy")).toBe("Happy")
+})
+
+test('should return true when given true as a parameter', () =>{
+    expect(returnInput(true)).toBe(true)
 })
